b0785875e126859f4d03e53aa6c0a58d4596dd12
Wcookson <145378598+Wcookson@users.noreply.github.com>
2023-12-04 18:12:15 +0000

    Add files via upload

---
+++ storage/framework/views/2ed8c1320e9b6d4e7e5bbccc706d5a58.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.jsx'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+  </head>
+  <body>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+  </body>
+</html><?php /**PATH C:\Users\Will\Desktop\Final Project\Project\program\2\laravelc6-main\resources\views/app.blade.php ENDPATH**/ ?>
